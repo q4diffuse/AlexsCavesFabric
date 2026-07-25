25335078e131b7e7036ffc1e26cd522c648b1a71
q4diffuse <163012625+q4diffuse@users.noreply.github.com>
2026-07-25 11:47:02 +0200

    chore: bump version to 2.0.2-6

--- src/main/java/com/github/alexmodguy/alexscaves/AlexsCaves.java
+++ src/main/java/com/github/alexmodguy/alexscaves/AlexsCaves.java
@@ -25,7 +25,7 @@ import java.util.List;
 public class AlexsCaves {
     public static final String MODID = "alexscaves";
     public static final Logger LOGGER = LogUtils.getLogger();
-    public static final String VERSION = "2.0.2-5-fabric";
+    public static final String VERSION = "2.0.2-6-fabric";
 
     public static CommonProxy PROXY = new CommonProxy();
 
